3cd8734b6b1aea1062292a0632da8137dae2278a
tomm-rodion <autovolna@gmail.com>
2022-10-09 12:28:06 +0300

    HW-08(+hw-02+hw-03)

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+// import Player from '@vimeo/player';
+// const iframe = document.querySelector('iframe');
 
+// const player = new Player('handstick', {
+//   id: 19231868,
+//   width: 640,
+// });
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+import Player from '@vimeo/player';
+import * as _ from 'lodash';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+console.log(player);
+
+player.on('timeupdate', _.throttle(saveData, 1000));
+
+function saveData(data) {
+  console.log(data);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('play', data => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (data.seconds !== savedTime && savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+});
+//test
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import { throttle } from 'lodash';
+const form = document.querySelector('.feedback-form');
+console.log(form);
+const inputEmail = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+const LOCALSTORAGE_KEY = 'feedback-form-state';
 
+function onFormInput(e) {
+  const formData = { email: inputEmail.value, message: textarea.value };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function valueStorage() {
+  const parselocaleStorageData = JSON.parse(
+    localStorage.getItem(LOCALSTORAGE_KEY)
+  );
+  if (parselocaleStorageData) {
+    inputEmail.value = parselocaleStorageData.email;
+    textarea.value = parselocaleStorageData.message;
+  } else {
+    inputEmail.value = '';
+    textarea.value = '';
+  }
+}
+
+valueStorage();
